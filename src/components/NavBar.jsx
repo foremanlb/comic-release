90d6e905641a8605d4b68eb8a1c8abc9ba42c1f9
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-03-30 12:18:07 -0700

    started css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './NavBar.css'
 
 export default function NavBar() {
   return (
-    <div>
-      <Link to='/'>
-        <div>Home</div>
-      </Link>
-      <Link to='/marvel'>
-      <div>Marvel</div>
-      </Link>
-      <Link to='/dc'>
-      <div>DC</div>
-      </Link>
-      <Link to='/indie'>
-      <div>Indie</div>
-      </Link>
-      <Link to='/month'>
-      <div>Month</div>
-      </Link>
+    <div className='navDiv'>
+      <ul className='nav'>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <li>Home</li>
+        </Link>
+        <Link to='/marvel' style={{ textDecoration: 'none' }}>
+          <li>Marvel</li>
+        </Link>
+        <Link to='/dc' style={{ textDecoration: 'none' }}>
+          <li>DC</li>
+        </Link>
+        <Link to='/indie' style={{ textDecoration: 'none' }}>
+          <li>Indie</li>
+        </Link>
+        <Link to='/month' style={{ textDecoration: 'none' }}>
+          <li>Month</li>
+        </Link>
+      </ul>
     </div>
   )
 }
